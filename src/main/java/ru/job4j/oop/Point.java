5b4c30c76bf6437f9f0c6bcaef71e64bfafc1bcb
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-27 00:50:36 +0500

    4. Расстояние между точками в трехмерном пространстве.[122643#291269]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -4,16 +4,26 @@ import static java.lang.Math.pow;
 
 public class Point {
     private int x;
-
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(that.x - this.x, 2) + pow(that.y - this.y, 2) + pow(that.z - this.z, 2));
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -2,9 +2,10 @@ package ru.job4j.oop;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class PointTest extends TestCase {
-
+    @Test
     public void testDistance() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
@@ -12,4 +13,14 @@ public class PointTest extends TestCase {
         Assert.assertEquals(dist, 2, 0.01);
 
     }
+
+
+        @Test
+        public void testDistance3d() {
+            Point a = new Point(0, 0, 0);
+            Point b = new Point(0, 2, 0);
+            double dist = a.distance(b);
+            Assert.assertEquals(dist, 2, 0.01);
+
+        }
 }
