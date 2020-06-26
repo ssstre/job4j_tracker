c1bcb5a1cd93f3036569c48317d94db7b6b6fda8
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-26 15:37:36 +0500

    2.2. Рефакторинг - Площадь треугольника.[122642#291268]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -2,15 +2,17 @@ package ru.job4j.oop;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class TriangleTest extends TestCase {
 
+    @Test
     public void testPeriod() {
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
-        Assert.assertEquals(rsl, 2, 0.01);
+        Assert.assertEquals(rsl, 8, 0.01);
     }
 }
