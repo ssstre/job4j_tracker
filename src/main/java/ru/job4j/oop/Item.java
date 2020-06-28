83f9cd3810fc325945b72fc8b5e66ca4da50c535
ssstre <65305795+ssstre@users.noreply.github.com>
2020-06-28 13:31:48 +0500

    2. Перегрузка конструктора.[173890#291283]

--- src/main/java/ru/job4j/oop/Item.java
+++ src/main/java/ru/job4j/oop/Item.java
@@ -1,6 +1,19 @@
 package ru.job4j.oop;
 
 public class Item {
+    private String id;
+    private String name;
+
+    public Item() {
+    }
+    public Item(String id) {
+        id = this.id;
+    }
+
+    public Item(String id, String name) {
+        id = this.id;
+        name = this.name;
+    }
     public static void main(String[] args) {
         Item item = new Item();
     }
