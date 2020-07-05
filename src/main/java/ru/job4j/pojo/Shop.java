00fa2fedf4592d5bb36ccc410fa8a80634e3fec3
ssstre <65305795+ssstre@users.noreply.github.com>
2020-07-06 00:06:41 +0500

    3. Удаление моделей из массива.[176470#291292]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+public class Shop {
+
+    public static int indexOfNull(Product[] products) {
+        int index = -1;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product == null) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
